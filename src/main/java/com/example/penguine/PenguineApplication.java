581c03142bc8c58d2a24fcbad6f3ae3e1cd3f864
kai_henry <zerozidi22@gmail.com>
2020-09-21 23:44:12 +0900

    #init

---
+++ src/main/java/com/example/penguine/PenguineApplication.java
@@ -0,0 +1,13 @@
+package com.example.penguine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PenguineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PenguineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/penguine/PenguineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.penguine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PenguineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
